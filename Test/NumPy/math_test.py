63f838ee575561d09bc123b929ab062a22113bbd
HeTao <hetao@helijia.com>
2018-07-11 11:00:35 +0800

    add subtract multiply divide

--- Test/NumPy/math_test.py
+++ Test/NumPy/math_test.py
@@ -4,5 +4,14 @@ import numpy as np
 x = np.array([1, 2, 3, 4])
 y = np.array([5.5, 6.5, 7.5, 8.5])
 
-print("x + y = ", x + y)
-print("add(x, y) = ", np.add(x, y))
+print("%20s %s" % ("x + y = ", (x + y)))
+print("%20s %s" % ("add(x, y) = ", np.add(x, y)))
+
+print("%20s %s" % ("x - y = ", (x - y)))
+print("%20s %s" % ("subtract(x, y) = ", np.subtract(x, y)))
+
+print("%20s %s" % ("x * y = ", (x * y)))
+print("%20s %s" % ("multiply(x, y) = ", np.multiply(x, y)))
+
+print("%20s %s" % ("x / y = ", (x / y)))
+print("%20s %s" % ("divide(x, y) = ", np.divide(x, y)))
