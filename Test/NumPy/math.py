8c7fba5cc7b282f4ae12f94ea9c32896183b0dda
HeTao <hetao@helijia.com>
2018-07-11 10:46:21 +0800

    修改命名

--- Test/NumPy/math.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-# add subtract multiply divide
-# x = np.array([1, 2, 3, 4])
-# y = np.array([5.5, 6.5, 7.5, 8.5])
-#
-# print("x + y = ", x + y)
-# print("add(x, y) = ", np.add(x, y))
